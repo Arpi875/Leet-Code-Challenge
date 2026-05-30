2d5e6e9ae463bfd28050766885e90fd4a7e24335
Arpi875 <bhardwajarpit407@gmail.com>
2026-05-30 12:25:47 +0530

    Time: 0 ms (100%), Space: 9.4 MB (10.98%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.c
+++ 0069-sqrtx/0069-sqrtx.c
@@ -1,17 +1,17 @@
 int mySqrt(int x) {
-    if (x == 0) return 0;   // base case
+    if (x == 0) return 0;   
     
     int left = 1, right = x;
     int ans = 0;
 
     while (left <= right) {
-        long mid = left + (right - left) / 2;  // avoid overflow
+        long mid = left + (right - left) / 2;  
 
         if (mid * mid == x) {
-            return (int)mid;  // exact square root
+            return (int)mid;  
         } else if (mid * mid < x) {
-            ans = (int)mid;   // store possible floor value
-            left = mid + 1;   // search higher
+            ans = (int)mid;   
+            left = mid + 1;   
         } else {
             right = mid - 1;  // search lower
         }
